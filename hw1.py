139f112a35cd855379eb357afd9add2e06e9e297
Seungsik Bae <0730bss@gmail.com>
2024-12-02 00:20:45 +0900

    None

--- hw1.py
+++ hw1.py
@@ -100,7 +100,10 @@ def viterbi(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+
+    return 0       
+
+
 
 def viterbi_ec(train, test):
 
@@ -111,7 +114,8 @@ def viterbi_ec(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+
+    raise NotImplementedError('viterbi_ec: not implemented')
+
 
 if __name__ == "__main__":
-        baseline('./data/brown-training.txt', './data/brown-test.txt')
